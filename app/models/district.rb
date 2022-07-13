fa6ca1c71f5e6261adcd7515cb240d19cd389744
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-13 18:01:37 +0800

    fix typo

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,10 +1,7 @@
 class District < ApplicationRecord
   validates_presence_of :code
   validates_presence_of :name
-  validates_presence_of :is_capital
-  validates_presence_of :is_city
-  validates_presence_of :is_municipality
 
   belongs_to :region
-  has_many :city_municipalitys
+  has_many :city_municipalities
 end
