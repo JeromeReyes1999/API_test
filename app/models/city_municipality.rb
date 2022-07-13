1df46c7aca08f5e3c1b5e16602bd177420d6d7ac
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-13 17:58:38 +0800

    I made province or district optional for city municipalities. add one-to-many relationship between city/municipalities and barangays

--- app/models/city_municipality.rb
+++ app/models/city_municipality.rb
@@ -2,6 +2,7 @@ class CityMunicipality < ApplicationRecord
   validates_presence_of :code
   validates_presence_of :name
 
-  belongs_to :province
-  belongs_to :district
+  belongs_to :province, optional: true
+  belongs_to :district, optional: true
+  has_many :barangays
 end
