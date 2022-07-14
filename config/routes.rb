7ed4e6fce760b857ed4057212fa98e7fbfc463e1
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-14 18:18:15 +0800

    add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :regions, only: :index, defaults: {format: :json}
+    resources :provinces, only: :index, defaults: {format: :json}
+    resources :districts, only: :index, defaults: {format: :json}
+    resources :city_municipalities, only: :index, defaults: {format: :json}
+    resources :barangays, only: :index, defaults: {format: :json}
+  end
 end
